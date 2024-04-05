42707abf2834dad6953fa1e801fa3824ed03514c
Gmakk <71002826+Gmakk@users.noreply.github.com>
2024-04-05 19:34:28 +0300

    intermediate result(does not work)

--- src/main/java/org/example/FormulaCalculator.java
+++ src/main/java/org/example/FormulaCalculator.java
@@ -1,49 +1,55 @@
 package org.example;
 
+import lombok.Data;
+import org.docx4j.wml.PPrBase;
+
+@Data
 public class FormulaCalculator {
+    //сохранять ли старую стилизацию - по умолчанию нет
+    private static Boolean saveOldStyle = false;
 
     //TODO: для выпадающего списка посмотреть возможность выводить имеющиеся в бд поля
-// data{
-//
-//    }
+    //откуда брать данные - ОБЯЗАТЕЛЬНЫЕ поля
+    private static String database = null;
+    private static String table = null;
+    private static String field = null;
+    private static String primaryKey = null;
 
-    private Boolean isHeader = null;
-    //куча полей null
-    //коструктор с несколькими полями
-    //метод для вычисления формулы с проверкой наличия полей
-    //метод для считывания формулы
-    public enum Style{
+    //форматирование - по умолчанию нет
+    private static Boolean isHeader = null;//является ли текст заголовком
+
+    //стилизация - по умолчанию нет
+    private static Boolean isCursive = null;//курсив
+    private static Boolean isBald = null;//выделен жирным
+    //TODO: валидация введенного цвета
+    //public static String highlighting = null;//желтые выделения   "Yellow", etc
+    //public static String color = null;//цвет текста  "Red", etc
 
 
+    //шрифты, размеры - ОБЯЗАТЕЛЬНО, если не указано сохранять стилизацию
+    private static String font = null;//название шрифта "Times New Roman", etc
+    private static Integer fontSize = null;//размер шрифта 12, etc
+
+    //форматирование абзаца - ОБЯЗАТЕЛЬНО, если не указано сохранять стилизацию
+//    PPrBase.Ind ind = factory.createPPrBaseInd();
+//    ind.setFirstLine(new BigInteger("709"));
+//    pPr.setInd(ind);
+    //private static Boolean hasIndent = false;//есть ли отступ в начале нового параграфа
+    //остальные свойства параграфа типа текст по ширине
+
+
+    private static final FormulaCalculator INSTANCE = new FormulaCalculator();
+
+
+    private FormulaCalculator(){
     }
 
-    public enum States {
-
-        MASSACHUSETTS("Massachusetts",  "MA",   true),
-        MICHIGAN     ("Michigan",       "MI",   false);
-    // all 50 of those
-
-        private final String full;
-        private final String abbr;
-        private final boolean originalColony;
-
-        private States(String full, String abbr, boolean originalColony) {
-            this.full = full;
-            this.abbr = abbr;
-            this.originalColony = originalColony;
-        }
-
-        public String getFullName() {
-            return full;
-        }
-
-        public String getAbbreviatedName() {
-            return abbr;
-        }
-
-        public boolean isOriginalColony(){
-            return originalColony;
-        }
+    public static FormulaCalculator getInstance(){
+        return INSTANCE;
     }
 
+
+    public String calculate(){
+        return null;
+    }
 }
